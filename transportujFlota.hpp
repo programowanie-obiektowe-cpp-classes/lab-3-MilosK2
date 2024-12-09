6d7921b478bb0fcb4f9791a27555dbd24f789df3
MilosK2 <jano9987@gmail.com>
2024-12-09 15:43:54 +0100

    Spoznienie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int przew_towar = 0;
+    int liczba_zaglowek = 0;
+    while (przew_towar < towar) {
+    Stocznia stocznia{};
+    Statek*  s1 = stocznia();
+    przew_towar += s1->transportuj();
+
+    bool zag = dynamic_cast< Zaglowiec* > (s1);
+
+    if (zag)
+        liczba_zaglowek++;
+
+    delete s1;
+    }
+    
+    return liczba_zaglowek;
 }
